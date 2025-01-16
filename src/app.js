1e15b7ae46903b0e273d91235f76cea1cbcdfa29
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2025-01-16 14:55:52 +0100

    documentation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const userRoutes = require("./routes/userroutes");
 
 const appRouter = express.Router();
+
+appRouter.use("/users", userRoutes);
 
 module.exports = appRouter;
