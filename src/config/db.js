f15d1cef5fe1edc6fdb2e9fa5d5f72399eb82958
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2025-01-17 07:32:30 +0100

    initial app push

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://ieadeleke:biochemistry@cluster0.yrxma.mongodb.net/aimortgage");
+        await mongoose.connect("mongodb+srv://ieadeleke:biochemistry@cluster0.yrxma.mongodb.net/loveblog");
         console.log("DB connected successfully")
     } catch (err) {
         console.log(err);
