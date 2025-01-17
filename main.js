50d5fcd4e9e571d8886c85da5f6bc9940e3e04d7
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2025-01-17 10:25:38 +0100

    initial app push

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const express = require("express");
 const connectToDB = require("./src/config/db");
 const { swaggerDocs, swaggerUi } = require('./swagger');
 const appRouter = require("./src/app");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 
 connectToDB();
 
+app.use(cors({
+    origin: "*"
+}))
 app.use("/api/v1", appRouter);
 // app.use("/users", userRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
